e82268b0f011740b302e99be7698e317a08c0a3b
l.m.jones@leeds.ac.ukgit config --global user.name l.m.jones@leeds.ac.ukgit config --global user.name l.m.jones@leeds.ac.uk <l.m.jones@leeds.ac.uk>
2017-07-05 13:38:51 +0100

    Updating script

--- 5_phylogenetic_analysis.R
+++ 5_phylogenetic_analysis.R
@@ -1,4 +1,23 @@
-#Package installation
+#Install CRAN Task View for phylogenetics 
 install.packages('ctv')
 library('ctv')
 install.views('Phylogenetics')
+update.views('Phylogenetics')
+
+## load ape
+library(ape)
+
+## simulate a phylogeny
+tree <- rtree(n = 20)
+plot(tree, edge.width = 2)
+
+library(msa)
+
+library('bioconductor')
+R.version
+
+source("http://www.bioconductor.org/biocLite.R")
+biocLite("msa")
+library(msa)
+
+getwd()
